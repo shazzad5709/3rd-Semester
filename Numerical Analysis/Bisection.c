6ad55bcd1188a71d771228e1279c43363ea681f2
shazzad5709 <shazzad1111@gmail.com>
2022-03-08 20:03:43 +0600

    Bisection

---
+++ Numerical Analysis/Bisection.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<math.h>
+#define EPSILON 1e-9
+
+double func(double x)
+{
+	return (x*x*x+4*x*x-10);
+}
+
+void bisection(double a, double b)
+{
+	double c=a;
+	int itr=0;
+
+	while ((b-a) >= EPSILON)
+	{
+	    itr++;
+		c=(a+b)/2;
+		if(fabs(func(c))<=EPSILON)
+			break;
+		else if (func(c)*func(a) < 0)
+			b=c;
+		else
+			a=c;
+	}
+	printf("The value of root is %.9lf\n", c);
+	printf("No. of iteration %d", itr);
+    return;
+}
+
+int main()
+{
+	double a=1, b=2;
+	bisection(a, b);
+	return 0;
+}
