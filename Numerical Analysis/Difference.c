ee644c63bb3b854d8bd894a163d8b4a12d7d47ce
shazzad5709 <62200344+shazzad5709@users.noreply.github.com>
2022-03-10 15:13:51 +0600

    Update Difference.c

--- Numerical Analysis/Difference.c
+++ Numerical Analysis/Difference.c
@@ -1,8 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#define N 300
 
-double x[201], f[201], dc[201], dm[201];
+double x[N+1], f[N+1], dc[N+1], dm[N+1];
 
 double F(double x)
 {
@@ -31,37 +32,46 @@ double central(double x2, double x0)
 
 int main()
 {
-	x[0]=0;
+	
 	double l=0, r=10;
-	int n=200;
-	x[n]=10;
+	int n=N;
+
+	
 	double h=(r-l)/(double)n;
+
+	x[0]=0;
 	f[0]=F(x[0]);
 	dc[0]=diff(x[0]);
-	x[n]=10;
-	f[n]=F(x[n]);
-	dc[n]=diff(x[n]);
-	for(int i=1; i<n; i++)
+	
+	for(int i=1; i<N; i++)
 	{
 		x[i]=x[i-1]+h;
 		f[i]=F(x[i]);
 		dc[i]=diff(x[i]);
 	}
-	// for(int i=0; i<n+1; i++)
-	// 	printf("%lf\n", f[i]);
+
+	x[N]=10;
+	f[N]=F(x[N]);
+	dc[N]=diff(x[N]);
+
+	
 	dm[0]=forward(x[0], x[1]);
-	dm[n]=backward(x[n], x[n-1]);
-	for(int i=1; i<n; i++)
+
+	for(int i=1; i<N; i++)
 	{
 		dm[i]=central(x[i+1], x[i-1]);
 	}
+
+	dm[N]=backward(x[N], x[N-1]);
+
 	double max=fabs(dm[0]-dc[0]);
-	for(int i=1; i<n+1; i++)
+	
+	for(int i=1; i<N+1; i++)
 	{
 		if(max<fabs(dm[i]-dc[i]))
 			max=fabs(dm[i]-dc[i]);
 	}
-	for(int i=0; i<3; i++)
-		printf("F=%lf D=%lf\n", dm[i], dc[i]);
+	// for(int i=0; i<3; i++)
+	// 	printf("F=%lf D=%lf\n", dm[i], dc[i]);
 	printf("Maximum: %lf\n", max);
 }
