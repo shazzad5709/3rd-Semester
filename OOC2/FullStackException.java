cf9a114390915e254d2ead2147cf293b13cb71bc
shazzad5709 <shazzad1111@gmail.com>
2022-03-19 11:16:57 +0600

    FullStackException

--- OOC2/FullStackException.java
+++ OOC2/FullStackException.java
@@ -1,3 +1,10 @@
 public class FullStackException extends RuntimeException {
+        public FullStackException() {
+            this("Stack is full");
+        }
+        
+        public FullStackException(String exception) {
+            super(exception);
+        }
         
 }
