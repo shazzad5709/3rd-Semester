f17a0624050b79ad05426357a4e4dcb7f696c1fd
shazzad5709 <shazzad1111@gmail.com>
2022-04-12 23:37:58 +0600

    kruskal

--- Algorithm/Graph/FloydWarshallAPSP.cpp
+++ Algorithm/Graph/FloydWarshallAPSP.cpp
@@ -1,16 +1,10 @@
-// C++ Program for Floyd Warshall Algorithm
 #include <bits/stdc++.h>
 using namespace std;
 
-// Number of vertices in the graph
 #define V 4
 
-/* Define Infinite as a large enough
-value.This value will be used for
-vertices not connected to each other */
 #define INF 99999
 
-// A function to print the solution matrix
 void printSolution(int dist[][V]);
 
 // Solves the all-pairs shortest path
@@ -43,18 +37,19 @@ void floydWarshall(int graph[][V])
 	vertex no. k is added to the set of
 	intermediate vertices and the set becomes {0, 1, 2, ..
 	k} */
-	for (k = 0; k < V; k++) {
+	for (k = 0; k < V; k++)
+	{
 		// Pick all vertices as source one by one
-		for (i = 0; i < V; i++) {
+		for (i = 0; i < V; i++)
+		{
 			// Pick all vertices as destination for the
 			// above picked source
-			for (j = 0; j < V; j++) {
+			for (j = 0; j < V; j++)
+			{
 				// If vertex k is on the shortest path from
 				// i to j, then update the value of
 				// dist[i][j]
-				if (dist[i][j] > (dist[i][k] + dist[k][j])
-					&& (dist[k][j] != INF
-						&& dist[i][k] != INF))
+				if (dist[i][j] > (dist[i][k] + dist[k][j]) && (dist[k][j] != INF && dist[i][k] != INF))
 					dist[i][j] = dist[i][k] + dist[k][j];
 			}
 		}
@@ -70,11 +65,13 @@ void printSolution(int dist[][V])
 	cout << "The following matrix shows the shortest "
 			"distances"
 			" between every pair of vertices \n";
-	for (int i = 0; i < V; i++) {
-		for (int j = 0; j < V; j++) {
+	for (int i = 0; i < V; i++)
+	{
+		for (int j = 0; j < V; j++)
+		{
 			if (dist[i][j] == INF)
 				cout << "INF"
-					<< "	 ";
+					 << "	 ";
 			else
 				cout << dist[i][j] << "	 ";
 		}
@@ -94,10 +91,10 @@ int main()
 	\|/	 |
 	(1)------->(2)
 			3	 */
-	int graph[V][V] = { { 0, 5, INF, 10 },
-						{ INF, 0, 3, INF },
-						{ INF, INF, 0, 1 },
-						{ INF, INF, INF, 0 } };
+	int graph[V][V] = {{0, 5, INF, 10},
+					   {INF, 0, 3, INF},
+					   {INF, INF, 0, 1},
+					   {INF, INF, INF, 0}};
 
 	// Print the solution
 	floydWarshall(graph);
--- Algorithm/Graph/KruskalMST.cpp
+++ Algorithm/Graph/KruskalMST.cpp
@@ -1,117 +1,73 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define ll long long
+const int N = 100;
+int parent[N];
 
+struct we_ed
+{
 
-class DSU {
-	int* parent;
-	int* rank;
-
-public:
-	DSU(int n)
-	{
-		parent = new int[n];
-		rank = new int[n];
-
-		for (int i = 0; i < n; i++) {
-			parent[i] = -1;
-			rank[i] = 1;
-		}
-	}
-
-	// Find function
-	int find(int i)
-	{
-		if (parent[i] == -1)
-			return i;
-
-		return parent[i] = find(parent[i]);
-	}
-	// union function
-	void unite(int x, int y)
-	{
-		int s1 = find(x);
-		int s2 = find(y);
-
-		if (s1 != s2) {
-			if (rank[s1] < rank[s2]) {
-				parent[s1] = s2;
-				rank[s2] += rank[s1];
-			}
-			else {
-				parent[s2] = s1;
-				rank[s1] += rank[s2];
-			}
-		}
-	}
+	int weight;
+	int node1;
+	int node2;
 };
+struct we_ed graph[N];
 
-class Graph {
-	vector<vector<int> > edgelist;
-	int V;
+bool cmp(struct we_ed a, struct we_ed b)
+{
+	return a.weight <= b.weight;
+}
+void make_set(int n)
+{
+	parent[n] = n;
+}
 
-public:
-	Graph(int V) { this->V = V; }
+int Find_set(int n)
+{
+	if (parent[n] == n)
+		return n;
+	return Find_set(parent[n]);
+}
 
-	void addEdge(int x, int y, int w)
+void Union(int n1, int n2)
+{
+	int parent_n1 = Find_set(n1);
+	int parent_n2 = Find_set(n2);
+	if (parent_n1 != parent_n2)
 	{
-		edgelist.push_back({ w, x, y });
+		parent[parent_n2] = parent_n1;
 	}
+}
 
-	void kruskals_mst()
-	{
-		// 1. Sort all edges
-		sort(edgelist.begin(), edgelist.end());
-
-		// Initialize the DSU
-		DSU s(V);
-		int ans = 0;
-		cout << "Following are the edges in the "
-				"constructed MST"
-			<< endl;
-		for (auto edge : edgelist) {
-			int w = edge[0];
-			int x = edge[1];
-			int y = edge[2];
-
-			// take that edge in MST if it does form a cycle
-			if (s.find(x) != s.find(y)) {
-				s.unite(x, y);
-				ans += w;
-				cout << x << " -- " << y << " == " << w
-					<< endl;
-			}
-		}
-		cout << "Minimum Cost Spanning Tree: " << ans;
-	}
-};
 int main()
 {
-	/* Let us create following weighted graph
-				10
-			0--------1
-			| \	 |
-			6| 5\ |15
-			|	 \ |
-			2--------3
-				4	 */
-	Graph g(4);
-	g.addEdge(0, 1, 10);
-	g.addEdge(1, 3, 15);
-	g.addEdge(2, 3, 4);
-	g.addEdge(2, 0, 6);
-	g.addEdge(0, 3, 5);
+	int node, edge;
+	cin >> node >> edge;
+	for (int i = 0; i < edge; i++)
+	{
+		int u, v, w;
+		cin >> u >> v >> w;
+		graph[i].weight = w;
+		graph[i].node1 = u;
+		graph[i].node2 = v;
+	}
 
-	// int n, m;
-	// cin >> n >> m;
+	for (int i = 0; i < node; i++)
+		make_set(i);
 
-	// Graph g(n);
-	// for (int i = 0; i < m; i++)
-	// {
-	//	 int x, y, w;
-	//	 cin >> x >> y >> w;
-	//	 g.addEdge(x, y, w);
-	// }
+	sort(graph, graph + edge, cmp);
 
-	g.kruskals_mst();
-	return 0;
+	int total_cost = 0;
+	cout << "MST\n";
+	for (int i = 0; i < edge; i++)
+	{
+		if (Find_set(graph[i].node1) != Find_set(graph[i].node2))
+		{
+			cout << (char)(graph[i].node1 + 97) << " " << (char)(graph[i].node2 + 97) << " " << graph[i].weight << endl;
+			Union(graph[i].node1, graph[i].node2);
+			total_cost += graph[i].weight;
+		}
+	}
+
+	cout << "Total Cost: " << total_cost;
 }
